dbcce7e99dfc5f5acfcc23349ef5d317446fe9a3
DarkMatter <zakygames701@gmail.com>
2022-09-11 23:58:12 +0100

    [~] update

--- app.js
+++ app.js
@@ -18,38 +18,44 @@ const notifier = require("yt-notifs");
 
 client.on("ready", () => {
     console.log("Watching " + config.CHANNEL_ID.length  + " Channels")
+    const YTIDArray = [];
     config.CHANNEL_ID.forEach(channel => {
         console.log("Name of channel: " + notifier.getChannelName(channel));
     });
-    client.user.setPresence({ activities: [{ name: "YouTube", type: ActivityType.Watching }], status: 'online'});
+    client.user.setPresence({ activities: [{ name: config.CHANNEL_ID.length  + " Channels", type: ActivityType.Watching }], status: 'online'});
 });
 
 notifier.start(60);
 
 notifier.events.on("newVid", (obj) => {
-    console.log('New Video');
-    // console.log(obj);
-    // send embed when video gets posted
-    let disc = obj.vid.description;
-    if(disc && disc.length > 100){
-        disc = disc.slice(0, 100) + "...";
+    if(YTIDArray.includes(obj.vid.id)){
+        return;
     }else{
-        disc = disc;
+        console.log('New Video');
+        // console.log(obj);
+        // send embed when video gets posted
+        let disc = obj.vid.description;
+        if(disc && disc.length > 100){
+            disc = disc.slice(0, 100) + "...";
+        }else{
+            disc = disc;
+        }
+        const embed = new EmbedBuilder()
+            .setTitle(obj.vid.name || "Could not find a title")
+            // .setDescription(obj.vid.description || "Could not find a description")
+            .addFields({
+                name: "Video Description",
+                value: disc || "Could not find a description"
+            })
+            .setColor("#c4302b")
+            .setThumbnail(obj.vid.thumbnail.url || null)
+            .setURL(obj.vid.url || null)
+            .setAuthor({ name: obj.channel.name || "Could not find a username", url: obj.channel.url || null })
+            .setTimestamp()
+        client.channels.cache.get(config.SERVER_CHANNEL_ID).send({embeds: [embed]});
+        YTIDArray.push(obj.vid.id)
+        // client.channels.cache.get(config.SERVER_CHANNEL_ID).send(`**${data.channel.name}** just uploaded a new video - **${data.video.link}**`);
     }
-    const embed = new EmbedBuilder()
-        .setTitle(obj.vid.name || "Could not find a title")
-        // .setDescription(obj.vid.description || "Could not find a description")
-        .addFields({
-            name: "Video Description",
-            value: disc || "Could not find a description"
-        })
-        .setColor("#c4302b")
-        .setThumbnail(obj.vid.thumbnail.url || null)
-        .setURL(obj.vid.url || null)
-        .setAuthor({ name: obj.channel.name || "Could not find a username", url: obj.channel.url || null })
-        .setTimestamp()
-    client.channels.cache.get(config.SERVER_CHANNEL_ID).send({embeds: [embed]});
-    // client.channels.cache.get(config.SERVER_CHANNEL_ID).send(`**${data.channel.name}** just uploaded a new video - **${data.video.link}**`);
 });
 
 notifier.subscribe(config.CHANNEL_ID);
